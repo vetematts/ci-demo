3a901384933e0b1863d4ea2ad8d1db14ac6f3e83
vetematts <165625991+vetematts@users.noreply.github.com>
2026-03-04 18:38:22 +1000

    fix: Add missing eslint-disable comments in test file

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,7 @@
 const sum = require('./sum')
 
+// eslint-disable-next-line no-undef
 test("add 1 and 2 which equals to 3", () => {
+    // eslint-disable-next-line no-undef
     expect(sum(1,2)).toBe(3);
 });
