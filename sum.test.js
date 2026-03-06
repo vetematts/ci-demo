66cf56062ff229acdc11e959ff7040d1e49e912a
vetematts <165625991+vetematts@users.noreply.github.com>
2026-03-07 09:11:47 +1000

    feat: Intentionally breaking the test file to see if the jobs workflow functions

--- sum.test.js
+++ sum.test.js
@@ -3,5 +3,5 @@ const sum = require('./sum')
 // eslint-disable-next-line no-undef
 test("add 1 and 2 which equals to 3", () => {
     // eslint-disable-next-line no-undef
-    expect(sum(1,2)).toBe(3);
+    expect(sum(1,2)).toBe(1);
 });
