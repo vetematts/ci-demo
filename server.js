0ee2208acd10379ca2894910f4609038f0937b3e
vetematts <165625991+vetematts@users.noreply.github.com>
2026-03-16 18:56:34 +1000

    feat: Defined server.js to make the appliation a web application

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
-const express = require('express');
-const sum = require('./sum');
+const express = require("express")
+const sum = require("./sum")
 
 const app = express();
 
-const VERSION = process.env.VERSION || "v1";
+// eslint-disable-next-line no-undef
+const VERSION = process.env.APP_VERSION || "v1";
+// eslint-disable-next-line no-undef
+const NAME = process.env.APP_NAME || "sum-service";
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3000;
 
 // URL /sum?a=1&b=2
-app.get('/sum', (req, res) => {
-// get the parameters
-const firstValue = Number(req.query.firstValue);
-const secondValue = Number(req.query.secondValue);
-//run the sum() function
-const result = sum(firstValue, secondValue);
-// return the output
-res.json({ output: result });
+app.get("/sum", (request, response) => {
+    // get the parameters
+    const firstValue = Number(request.query.firstValue);
+    const secondValue = Number(request.query.secondValue);
+
+    // run the sum() function
+    const result = sum(firstValue, secondValue);
+
+    // return the output
+    response.json({
+        service: NAME,
+        version: VERSION,
+        output: result
+    });
 });
 
-app.get("/version", (req, res) => {
-    res.json({ version: VERSION });
+app.get("/version", (request, response) => {
+    response.json({
+        version: VERSION || "v1"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`${NAME} running. Version: ${VERSION}. PORT: ${PORT}`);
 });
